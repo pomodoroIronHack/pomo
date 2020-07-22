7acbc00e0842bf5d8c0b4c242552666e8f46b398
Anna Weingart <annaraewein@gmail.com>
2020-07-22 13:36:24 +0200

    removed console log test

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-  console.log(mongoose)
+ 
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
