44fca01c3ac79c95e8207dcd47a62919482de52b
Anna Weingart <annaraewein@gmail.com>
2020-07-22 13:35:04 +0200

    console log test

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
+  console.log(mongoose)
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
