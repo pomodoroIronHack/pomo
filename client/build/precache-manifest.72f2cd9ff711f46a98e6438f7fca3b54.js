89c67daa1391b07075243af18b41f40dd03521b2
Anna Weingart <annaraewein@gmail.com>
2020-07-30 17:59:47 +0200

    heroku deploy

---
+++ client/build/precache-manifest.72f2cd9ff711f46a98e6438f7fca3b54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8882e23a43d774fe597e9cf63604dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00c233add44973e174a4",
+    "url": "/static/css/2.14e36ea5.chunk.css"
+  },
+  {
+    "revision": "66abb1750c3e8413dca2",
+    "url": "/static/css/main.69ac9a3c.chunk.css"
+  },
+  {
+    "revision": "00c233add44973e174a4",
+    "url": "/static/js/2.76040fed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.76040fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66abb1750c3e8413dca2",
+    "url": "/static/js/main.a9afe1a1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
