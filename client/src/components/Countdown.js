bb619fa0dd5456c0a777f03a675dceaa34af23c9
Anna Weingart <annaraewein@gmail.com>
2020-07-30 12:34:46 +0200

    back end changes

--- client/src/components/Countdown.js
+++ client/src/components/Countdown.js
@@ -1,78 +1,83 @@
 import React, { Component } from "react";
 import * as Icon from "react-feather";
-import FixedNavbar from './FixedNavbar'
-import { Navbar as Nav } from 'react-bootstrap';
+import FixedNavbar from "./FixedNavbar";
+import { Navbar as Nav } from "react-bootstrap";
 
-const logicalFlow = [ 25,  5,  25,  5, 25,  20]
+const logicalFlow = [25, 5, 25, 5, 25, 20];
 // const logicalFlow = [ 1,  2,  3,  4, 5,  6]
 
 export default class Countdown extends Component {
-
   state = {
     timerOn: false,
     timerStart: 0,
     timerTime: 0,
     seconds: "00",
     timer: null,
-    minutes: 25
+    minutes: 25,
   };
 
   startTimer = () => {
-    this.setState( {
+    this.setState({
       timerOn: true,
-timer: setInterval(()=>{
-    
-  if (!this.state.timerOn) {
-    this.setState( {
-      timerOn: true
-    })
-    return
-  }
-  if (this.state.timerOn) {
-    
-  console.log(this.state.timerTime)
-  if (this.state.seconds === "00" && this.state.minutes >= 1) {
-    return this.setState({minutes: this.state.minutes -1, seconds: 59})
-  }
-  if (this.state.seconds === 0 && this.state.minutes >0) {
-    this.setState({
-      minutes: this.state.minutes -1, seconds: 59
-    })
-  }
-  if (this.state.seconds >=1) {
-    this.setState({
-      seconds: this.state.seconds -1
-    })
-  }
-  if (this.state.seconds === 0 && this.state.minutes === 0 ) {
-    this.setState({
-      timerStart: this.state.timerStart == logicalFlow.length -1? 0 : this.state.timerStart +1
-    }, ()=> {
-      this.setState({
-        minutes: logicalFlow[this.state.timerStart] - 1, seconds: 59
-      })
-    })
-  }
-}
-  }, 20 )
-    })
-    
-  }
-
+      timer: setInterval(() => {
+        if (!this.state.timerOn) {
+          this.setState({
+            timerOn: true,
+          });
+          return;
+        }
+        if (this.state.timerOn) {
+          console.log(this.state.timerTime);
+          if (this.state.seconds === "00" && this.state.minutes >= 1) {
+            return this.setState({
+              minutes: this.state.minutes - 1,
+              seconds: 59,
+            });
+          }
+          if (this.state.seconds === 0 && this.state.minutes > 0) {
+            this.setState({
+              minutes: this.state.minutes - 1,
+              seconds: 59,
+            });
+          }
+          if (this.state.seconds >= 1) {
+            this.setState({
+              seconds: this.state.seconds - 1,
+            });
+          }
+          if (this.state.seconds === 0 && this.state.minutes === 0) {
+            this.setState(
+              {
+                timerStart:
+                  this.state.timerStart == logicalFlow.length - 1
+                    ? 0
+                    : this.state.timerStart + 1,
+              },
+              () => {
+                this.setState({
+                  minutes: logicalFlow[this.state.timerStart] - 1,
+                  seconds: 59,
+                });
+              }
+            );
+          }
+        }
+      }, 20),
+    });
+  };
 
   stopTimer = () => {
     clearInterval(this.state.timer);
     this.setState({ timerOn: false });
   };
-  
 
   resetTimer = () => {
     // return this.stopTimer()
-    this.stopTimer()
+    this.stopTimer();
     this.setState({
-      minutes: logicalFlow[this.state.timerStart], seconds: "00",
-
-    })
+      minutes: logicalFlow[this.state.timerStart],
+      seconds: "00",
+    });
     // if (this.state.timerOn === false) {
     //   this.stopTimer();
     //   this.setState({
@@ -81,30 +86,30 @@ timer: setInterval(()=>{
     // }
   };
 
-  
+  render() {
+    const { timerTime, timerStart, timerOn } = this.state;
+    let seconds = ("0" + (Math.floor((timerTime / 1000) % 60) % 60)).slice(-2);
+    let minutes = ("0" + Math.floor((timerTime / 60000) % 60)).slice(-2);
 
-    render() {
-
-      const { timerTime, timerStart, timerOn } = this.state;
-      let seconds = ("0" + (Math.floor((timerTime / 100) % 60) % 60)).slice(-2);
-      let minutes = ("0" + Math.floor((timerTime / 6000) % 60)).slice(-2);
-  
-      return (
-          <div className='divdiv'>
-          <div className="Countdown">
-            
-            {/* <button onClick={this.resetTimer}> CLICK TO RESET</button> */}
+    return (
+      <div className="divdiv">
+        <div className="Countdown">
           <div className="Countdown-time">
-            {this.state.minutes} : {this.state.seconds}  
+            {this.state.minutes} : {this.state.seconds}
           </div>
-          <button className='Button-start' onClick={this.state.timerOn ? this.stopTimer : this.startTimer}> {this.state.timerOn ? <Icon.Pause/> : <Icon.Play/>}</button>
+          <button
+            className="Button-start"
+            onClick={this.state.timerOn ? this.stopTimer : this.startTimer}
+          >
+            {" "}
+            {this.state.timerOn ? <Icon.Pause /> : <Icon.Play />}
+          </button>
         </div>
-        <FixedNavbar/>
-        </div>
-      );
-    }
+        <button className="btn-reset" onClick={this.resetTimer}>
+          RESET
+        </button>
+        <FixedNavbar />
+      </div>
+    );
   }
-  
-  
-
-    
+}
--- routes/list.js
+++ routes/list.js
@@ -1,56 +1,58 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const List = require('../models/List');
-const Task = require('../models/Task');
-router.post('/lists', (req, res) => {
+const List = require("../models/List");
+const Task = require("../models/Task");
+
+router.post("/lists", (req, res) => {
+  console.log("hello");
   // const { title, description, tasks = [] } = req.body;
   const title = req.body.title;
   const description = req.body.description;
   const tasks = [];
-  const owner = req.user._id;
+  // const owner = req.user._id;
   List.create({
     title: title,
     description: description,
     tasks: tasks,
-    owner: owner
+    // owner: owner,
   })
-    .then(list => {
+    .then((list) => {
       res.status(201).json(list);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 // GET /api/lists
-router.get('/lists', (req, res) => {
+router.get("/lists", (req, res) => {
   List.find()
-    .populate('tasks')
-    .then(lists => {
+    .populate("tasks")
+    .then((lists) => {
       res.status(200).json(lists);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 // GET /api/lists/:id
 // return a specific `list` resource with a given id
-router.get('/lists/:id', (req, res) => {
+router.get("/lists/:id", (req, res) => {
   // check if req.params.id is valid, if not respond with a 4xx status code
   List.findById(req.params.id)
-    .populate('tasks')
-    .then(list => {
+    .populate("tasks")
+    .then((list) => {
       if (!list) {
         res.status(404).json(list);
       } else {
         res.status(200).json(list);
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 // PUT /api/lists/:id
-router.put('/lists/:id', (req, res) => {
+router.put("/lists/:id", (req, res) => {
   const { title, description } = req.body;
   List.findByIdAndUpdate(
     req.params.id,
@@ -58,34 +60,25 @@ router.put('/lists/:id', (req, res) => {
     // { new: true } ensures that we are getting the updated document in the .then callback
     { new: true }
   )
-    .then(list => {
+    .then((list) => {
       res.status(200).json(list);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 // DELETE /api/lists/:id
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete the list
   List.findByIdAndDelete(req.params.id)
-    .then(list => {
+    .then((list) => {
       // Deletes all the documents in the Task collection where the value for the `_id` field is present in the `list.tasks` array
       return Task.deleteMany({ _id: { $in: list.tasks } }).then(() => {
-        res.status(200).json({ message: 'ok' });
+        res.status(200).json({ message: "ok" });
       });
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 module.exports = router;
-
-
-
-
-
-
-
-
-
