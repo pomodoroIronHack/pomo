c1b85dd179cacedba2425b212a042a4c03136cb5
Anna Weingart <annaraewein@gmail.com>
2021-02-03 20:16:26 +0100

    stack overflow

--- app.js
+++ app.js
@@ -65,25 +65,12 @@ app.use(
   })
 );
 
-
-const root = require('path').join(__dirname, 'client', 'build')
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(root));
-  app.get("*", (req, res) => {
-    res.sendFile('index.html', { root });
-})
-//   app.use(express.static('client/build'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(require('path')
-//     .resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
-// app.get("*", (req, res) => {
-//   res.sendFile(require('path')
-//       .resolve(__dirname, 'client', 'build', 'index.html'));
-// })
-
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //original 
 
@@ -95,9 +82,9 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-// +app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-// });
++app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 // default value for title local
 // app.locals.title = 'Express - Generated with IronGenerator';
