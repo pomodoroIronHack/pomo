2959f946f8cdb6542a9ae5a47d59149dd2186008
Anna Weingart <annaraewein@gmail.com>
2021-02-05 19:54:29 +0100

    Refactored code

--- app.js
+++ app.js
@@ -11,45 +11,69 @@ const path = require("path");
 const uri = process.env.MONGODB_URI;
 
 mongoose
+
   .connect( uri|| "mongodb://localhost/list-x", {
+
     useNewUrlParser: true, useUnifiedTopology: true,
+
   })
+
   .then((x) => {
+
     console.log(
+
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
+
     );
+
   })
+
   .catch((err) => {
+
     console.error("Error connecting to mongo", err);
+
   });
 
-  const app_name = require("./package.json").name;
+const app_name = require("./package.json").name;
+
 const debug = require("debug")(
+
   `${app_name}:${path.basename(__filename).split(".")[0]}`
+
 );
 
 const app = express();
-
 const session = require("express-session");
 const passport = require("passport");
 const cors = require("cors");
+
 app.use(cors());
 
 require("./configs/passport");
 
 const MongoStore = require("connect-mongo")(session);
+
 app.use(
+
   session({
+
     secret: process.env.SESSION_SECRET,
+
     resave: false,
+
     saveUninitialized: false,
+
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
+
   })
+
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Middleware Setup
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -58,108 +82,37 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(
+
   require("node-sass-middleware")({
+
     src: path.join(__dirname, "public"),
+
     dest: path.join(__dirname, "public"),
+
     sourceMap: true,
+
   })
+
 );
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
-
-// default value for title local
-// app.locals.title = 'Express - Generated with IronGenerator';
-
 app.use("/api/", require("./routes/list"));
 app.use("/api/tasks", require("./routes/task"));
 app.use("/api/auth", require("./routes/auth"));
 
 const index = require("./routes/index");
+
 app.use("/", index);
+
 app.use((req, res) => {
+
   // If no routes match, send them the React HTML.
+
   res.sendFile(__dirname + "/client/build/index.html");
+
 });
+
 module.exports = app;
-
-
-// const app_name = require("./package.json").name;
-// const debug = require("debug")(
-//   `${app_name}:${path.basename(__filename).split(".")[0]}`
-// );
-
-// const app = express();
-
-// const session = require("express-session");
-// const passport = require("passport");
-// const cors = require("cors");
-// app.use(cors());
-
-// require("./configs/passport");
-
-// const MongoStore = require("connect-mongo")(session);
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-//   })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Middleware Setup
-// app.use(logger("dev"));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// // Express View engine setup
-
-// app.use(
-//   require("node-sass-middleware")({
-//     src: path.join(__dirname, "public"),
-//     dest: path.join(__dirname, "public"),
-//     sourceMap: true,
-//   })
-// );
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
-// //original 
-
-// //new from express website 
-
-
-
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "hbs");
-// app.use(express.static(path.join(__dirname, "/client/build")));
-
-// +app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-// });
-
-// // default value for title local
-// // app.locals.title = 'Express - Generated with IronGenerator';
-
-// app.use("/api/", require("./routes/list"));
-// app.use("/api/tasks", require("./routes/task"));
-// app.use("/api/auth", require("./routes/auth"));
-
-// const index = require("./routes/index");
-// app.use("/", index);
-// app.use((req, res) => {
-//   // If no routes match, send them the React HTML.
-//   res.sendFile(__dirname + "/client/build/index.html");
-// });
-// module.exports = app;
--- routes/task.js
+++ routes/task.js
@@ -2,60 +2,115 @@ const express = require('express');
 const Task = require('../models/Task');
 const List = require('../models/List');
 const router = express.Router();
+
 router.get('/:id', (req, res) => {
+
   const id = req.params.id;
+
   Task.findById(id)
+
     .then(task => {
+
       res.status(200).json(task);
+
     })
+
     .catch(err => {
+
       res.json(err);
+
     });
+
 });
+
 router.post('/', (req, res) => {
+
   const { title, description, listId } = req.body;
+
   console.log('task');
+
   Task.create({
+
     title,
+
     description,
+
     list: listId
+
   })
+
     .then(task => {
+
       return List.findByIdAndUpdate(listId, {
+
         $push: { tasks: task._id }
+
       }).then(() => {
+
         res.status(201).json({
+
           message: `Task with id ${task._id} was successfully added to list with id ${listId}`
+
         });
+
       });
+
     })
+
     .catch(err => {
+
       res.json(err);
+
     });
+
 });
+
 router.put('/:id', (req, res, next) => {
+
   const id = req.params.id;
+
   const { title, description } = req.body;
+
   Task.findByIdAndUpdate(id, { title, description }, { new: true })
+
     .then(task => {
+
       res.json(task);
+
     })
     .catch(err => {
+
       res.json(err);
+
     });
+
 });
+
 router.delete('/:id', (req, res, next) => {
+
   const id = req.params.id;
+
   Task.findByIdAndDelete(id)
+
     .then(task => {
+
       return List.findByIdAndUpdate(task.list, {
+
         $pull: { tasks: id }
+
       }).then(() => {
+
         res.json({ message: 'ok' });
+
       });
+
     })
     .catch(err => {
+
       res.json(err);
+
     });
+
 });
+
 module.exports = router;
